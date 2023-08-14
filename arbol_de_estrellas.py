e29fe0351ee6b5b32f893e6a69fe7486f23e4537
GermanAndPer <germanandresperez1@gmail.com>
2023-08-13 19:18:52 -0600

    refactor se actualiza el codigo. Se simplifica. version_2

--- arbol_de_estrellas.py
+++ arbol_de_estrellas.py
@@ -1,12 +1,6 @@
-
 niveles = 0
-estrellas = '⭐'
-conteo = 1
 base = int(input('¿La base de cuantas estrellas quieres que sea?'))
 while base > niveles:
-    print(' '*int((base/2)), '*'*conteo,' '*int((base/2)))
-    conteo += 1
+    print(' '*int((base)), '⭐'*niveles,' '*int((base)))
     base -=1
     niveles += 1
-    if base == niveles:
-        break
